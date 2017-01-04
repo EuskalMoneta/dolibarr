e16de39603f9145688152f8adf48dcfabc87bd98
Xebax <xebax@wanadoo.fr>
2017-01-04 07:36:01 +0100

    Add a box "Ouvertures de compte" in the home screen.

---
+++ htdocs/core/boxes/box_ouvertures_de_compte.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Ce module est utilisé pour afficher une boite "Ouvertures de compte"
+ * dans la page d'accueil.
+ */
+
+include_once DOL_DOCUMENT_ROOT.'/core/boxes/modules_boxes.php';
+
+
+class box_ouvertures_de_compte extends ModeleBoxes
+{
+	var $boxcode="ouverture_compte";
+	var $boximg="object_generic";
+	var $boxlabel="Ouvertures de compte Eusko numérique";
+	var $depends = array("adherent");
+
+	var $db;
+	var $param;
+
+	var $info_box_head = array();
+	var $info_box_contents = array();
+
+
+	/**
+	 *  Constructor
+	 *
+	 *  @param  DoliDB	$db      	Database handler
+	 *  @param	string	$param		More parameters
+	 */
+	function __construct($db,$param='')
+	{
+		global $conf, $user;
+
+		$this->db = $db;
+
+		// disable module for such cases
+		$listofmodulesforexternal=explode(',',$conf->global->MAIN_MODULES_FOR_EXTERNAL);
+		if (! in_array('adherent',$listofmodulesforexternal) && ! empty($user->societe_id)) $this->enabled=0;	// disabled for external users
+	}
+
+	/**
+	 *  Load data into info_box_contents array to show array later.
+	 *
+	 *  @return	void
+	 */
+	function loadBox()
+	{
+		global $user, $langs, $db, $conf;
+		$langs->load("boxes");
+		$langs->load("box_ouvertures_de_compte");
+
+		$this->info_box_head = array('text' => $langs->trans("BoxTitleOuverturesCompte"));
+
+		if ($user->rights->adherent->lire)
+		{
+			$sql_comptes = "SELECT COUNT(*) AS nb";
+			$sql_comptes.= " FROM ".MAIN_DB_PREFIX."adherent_extrafields";
+			$sql_comptes.= " WHERE documents_pour_ouverture_du_compte_valides = 1";
+
+			$result_comptes = $db->query($sql_comptes);
+
+			$sql_prelevements = "SELECT COUNT(*) AS nb, SUM(prelevement_change_montant) AS total";
+			$sql_prelevements.= " FROM ".MAIN_DB_PREFIX."adherent_extrafields";
+			$sql_prelevements.= " WHERE prelevement_change_montant > 0";
+
+			$result_prelevements = $db->query($sql_prelevements);
+
+			if ($result_comptes && $result_prelevements)
+			{
+				$obj_comptes = $db->fetch_object($result_comptes);
+				$obj_prelevements = $db->fetch_object($result_prelevements);
+
+				$this->info_box_contents[0][] = array(
+					'td' => 'align="left"',
+					'text' => $langs->trans("NombreComptesOuverts"),
+				);
+
+				$this->info_box_contents[0][] = array(
+					'td' => 'align="right"',
+					'text' => $obj_comptes->nb,
+				);
+
+				$this->info_box_contents[1][] = array(
+					'td' => 'align="left"',
+					'text' => $langs->trans("NombrePrelevementsAuto"),
+				);
+
+				$this->info_box_contents[1][] = array(
+					'td' => 'align="right"',
+					'text' => $obj_prelevements->nb,
+				);
+
+				$this->info_box_contents[2][] = array(
+					'td' => 'align="left"',
+					'text' => $langs->trans("TotalDesChanges"),
+				);
+
+				$this->info_box_contents[2][] = array(
+					'td' => 'align="right"',
+					'text' => $obj_prelevements->total,
+				);
+
+				$this->info_box_contents[3][] = array(
+					'td' => 'align="left"',
+					'text' => $langs->trans("MoyenneDesChanges"),
+				);
+
+				$this->info_box_contents[3][] = array(
+					'td' => 'align="right"',
+					'text' => round($obj_prelevements->total/$obj_prelevements->nb, 2),
+				);
+
+				$db->free($result);
+			} else {
+				$this->info_box_contents[0][0] = array(
+					'td' => 'align="left"',
+					'maxlength'=>500,
+					'text' => ($db->error().' sql='.$sql),
+				);
+			}
+		} else {
+			$this->info_box_contents[0][0] = array(
+				'align' => 'left',
+				'text' => $langs->trans("ReadPermissionNotAllowed"),
+			);
+		}
+	}
+
+	/**
+	 *	Method to show box
+	 *
+	 *	@param	array	$head       Array with properties of box title
+	 *	@param  array	$contents   Array with properties of box lines
+	 *	@return	void
+	 */
+	function showBox($head = null, $contents = null)
+	{
+		parent::showBox($this->info_box_head, $this->info_box_contents);
+	}
+
+}
+
