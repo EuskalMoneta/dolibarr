87ce8cdfee66eb845d9321c2dc07c456b2590252
Xebax <xebax@wanadoo.fr>
2017-01-30 10:26:36 +0100

    Distinction des utilisateurs et des prestataires dans la boite  "Ouvertures de compte".

--- htdocs/core/boxes/box_ouvertures_de_compte.php
+++ htdocs/core/boxes/box_ouvertures_de_compte.php
@@ -54,10 +54,14 @@ class box_ouvertures_de_compte extends ModeleBoxes
 		if ($user->rights->adherent->lire)
 		{
 			$sql_comptes = "SELECT COUNT(*) AS nb";
-			$sql_comptes.= " FROM ".MAIN_DB_PREFIX."adherent_extrafields";
-			$sql_comptes.= " WHERE documents_pour_ouverture_du_compte_valides = 1";
+			$sql_comptes.= " FROM ".MAIN_DB_PREFIX."adherent adh";
+			$sql_comptes.= " JOIN ".MAIN_DB_PREFIX."adherent_extrafields adh_extra";
+			$sql_comptes.= " ON adh.rowid = adh_extra.fk_object";
+			$sql_comptes.= " WHERE adh_extra.documents_pour_ouverture_du_compte_valides = 1";
+			$sql_comptes.= " AND adh.login LIKE ";
 
-			$result_comptes = $db->query($sql_comptes);
+			$result_comptes_prestataires = $db->query($sql_comptes . "'Z%'");
+			$result_comptes_utilisateurs = $db->query($sql_comptes . "'E%'");
 
 			$sql_prelevements = "SELECT COUNT(*) AS nb, SUM(prelevement_change_montant) AS total";
 			$sql_prelevements.= " FROM ".MAIN_DB_PREFIX."adherent_extrafields";
@@ -65,52 +69,67 @@ class box_ouvertures_de_compte extends ModeleBoxes
 
 			$result_prelevements = $db->query($sql_prelevements);
 
-			if ($result_comptes && $result_prelevements)
+			if ($result_comptes_prestataires && $result_comptes_utilisateurs && $result_prelevements)
 			{
-				$obj_comptes = $db->fetch_object($result_comptes);
+				$obj_comptes_prestataires = $db->fetch_object($result_comptes_prestataires);
+				$obj_comptes_utilisateurs = $db->fetch_object($result_comptes_utilisateurs);
 				$obj_prelevements = $db->fetch_object($result_prelevements);
 
-				$this->info_box_contents[0][] = array(
+				$ligne = 0;
+
+				$this->info_box_contents[$ligne][] = array(
 					'td' => 'align="left"',
-					'text' => $langs->trans("NombreComptesOuverts"),
+					'text' => $langs->trans("NombreComptesOuvertsPrestataires"),
 				);
 
-				$this->info_box_contents[0][] = array(
+				$this->info_box_contents[$ligne][] = array(
 					'td' => 'align="right"',
-					'text' => $obj_comptes->nb,
+					'text' => $obj_comptes_prestataires->nb,
 				);
 
-				$this->info_box_contents[1][] = array(
+				$this->info_box_contents[++$ligne][] = array(
+					'td' => 'align="left"',
+					'text' => $langs->trans("NombreComptesOuvertsUtilisateurs"),
+				);
+
+				$this->info_box_contents[$ligne][] = array(
+					'td' => 'align="right"',
+					'text' => $obj_comptes_utilisateurs->nb,
+				);
+
+				$this->info_box_contents[++$ligne][] = array(
 					'td' => 'align="left"',
 					'text' => $langs->trans("NombrePrelevementsAuto"),
 				);
 
-				$this->info_box_contents[1][] = array(
+				$this->info_box_contents[$ligne][] = array(
 					'td' => 'align="right"',
 					'text' => $obj_prelevements->nb,
 				);
 
-				$this->info_box_contents[2][] = array(
+				$this->info_box_contents[++$ligne][] = array(
 					'td' => 'align="left"',
 					'text' => $langs->trans("TotalDesChanges"),
 				);
 
-				$this->info_box_contents[2][] = array(
+				$this->info_box_contents[$ligne][] = array(
 					'td' => 'align="right"',
 					'text' => $obj_prelevements->total,
 				);
 
-				$this->info_box_contents[3][] = array(
+				$this->info_box_contents[++$ligne][] = array(
 					'td' => 'align="left"',
 					'text' => $langs->trans("MoyenneDesChanges"),
 				);
 
-				$this->info_box_contents[3][] = array(
+				$this->info_box_contents[$ligne][] = array(
 					'td' => 'align="right"',
 					'text' => round($obj_prelevements->total/$obj_prelevements->nb, 2),
 				);
 
-				$db->free($result);
+				$db->free($result_comptes_prestataires);
+				$db->free($result_comptes_utilisateurs);
+				$db->free($result_prelevements);
 			} else {
 				$this->info_box_contents[0][0] = array(
 					'td' => 'align="left"',
