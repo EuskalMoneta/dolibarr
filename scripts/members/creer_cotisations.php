9ea4cdbb8dcf4e406b6b94d0d0b8742b818f5a9d
Xebax <xebax@wanadoo.fr>
2017-11-28 23:15:19 +0100

    Gérer les cotisations offertes et les cotisations payées par prélèvement en eusko.

--- scripts/members/creer_cotisations.php
+++ scripts/members/creer_cotisations.php
@@ -50,119 +50,155 @@ if ($num_adherent == "" || $annee == "") {
 	return 1;
 }
 
+// Libellé des cotisations : toutes les cotisations sont pour l'année
+// indiquée en paramètre.
+$label = "Adhésion/cotisation $annee";
+
 // Start of transaction
 $db->begin();
 
-// Charger l'adhérent dans la base et vérifier les infos qui
-// concernent la cotisation par prélèvement automatique.
+try {
+
+// Charger l'adhérent dans la base et récupérer les infos qui concernent
+// la cotisation : cotisation offerte, prélèvement automatique, etc.
 $adherent = new Adherent($db);
 $adherent->fetch_login($num_adherent);
 if ($adherent->id <= 0) {
-	print "Erreur : l'adhérent $num_adherent n'existe pas.\n";
-	return 1;
+	throw new Exception("Erreur : l'adhérent $num_adherent n'existe pas.\n");
 }
-$prelevement_auto_cotisation = boolval($adherent->array_options["options_prelevement_auto_cotisation"]);
+$cotisation_offerte = boolval($adherent->array_options["options_cotisation_offerte"]);
+$prelevement_auto_cotisation_euro = boolval($adherent->array_options["options_prelevement_auto_cotisation"]);
+$prelevement_auto_cotisation_eusko = boolval($adherent->array_options["options_prelevement_auto_cotisation_eusko"]);
 $montant = floatval($adherent->array_options["options_prelevement_cotisation_montant"]);
 $periodicite = intval($adherent->array_options["options_prelevement_cotisation_periodicite"]);
-if (!$prelevement_auto_cotisation) {
-	print "Erreur : cet adhérent ($num_adherent) n'a pas choisi le prélèvement automatique (en €) pour le paiement de sa cotisation.";
-	return 1;
-}
-if ($montant <= 0 || !in_array($periodicite, array(1, 3, 6, 12))) {
-	print "Erreur : le montant ($montant) ou la périodicité ($periodicite) du prélèvement est incorrect.\n";
-	return 1;
-}
-$annee_fin_adhesion = getdate($adherent->datefin)["year"];
-if (intval($annee) <= $annee_fin_adhesion) {
-	print "Erreur : cet adhérent ($num_adherent)  est déjà à jour de cotisation jusqu'à fin $annee_fin_adhesion.\n";
-	return 1;
-}
 
-// Tous les paramètres ont été validés donc on crée les cotisations.
-// La liste des cotisations à créer dépend de la périodicité choisie
-// (1 = mensuel, 3 = trimestriel, 6 = semestriel, 12 = annuel).
-if ($periodicite === 1) {
-	$cotisations_a_creer = array(
-		array("debut" => "01/01/".$annee, "fin" => "31/01/".$annee),
-		array("debut" => "01/02/".$annee, "fin" => "28/02/".$annee),
-		array("debut" => "01/03/".$annee, "fin" => "31/03/".$annee),
-		array("debut" => "01/04/".$annee, "fin" => "30/04/".$annee),
-		array("debut" => "01/05/".$annee, "fin" => "31/05/".$annee),
-		array("debut" => "01/06/".$annee, "fin" => "30/06/".$annee),
-		array("debut" => "01/07/".$annee, "fin" => "31/07/".$annee),
-		array("debut" => "01/08/".$annee, "fin" => "31/08/".$annee),
-		array("debut" => "01/09/".$annee, "fin" => "30/09/".$annee),
-		array("debut" => "01/10/".$annee, "fin" => "31/10/".$annee),
-		array("debut" => "01/11/".$annee, "fin" => "30/11/".$annee),
-		array("debut" => "01/12/".$annee, "fin" => "31/12/".$annee),
-	);
-} else if ($periodicite === 3) {
-	$cotisations_a_creer = array(
-		array("debut" => "01/01/".$annee, "fin" => "31/03/".$annee),
-		array("debut" => "01/04/".$annee, "fin" => "30/06/".$annee),
-		array("debut" => "01/07/".$annee, "fin" => "30/09/".$annee),
-		array("debut" => "01/10/".$annee, "fin" => "31/12/".$annee),
-	);
-} else if ($periodicite === 6) {
-	$cotisations_a_creer = array(
-		array("debut" => "01/01/".$annee, "fin" => "30/06/".$annee),
-		array("debut" => "01/07/".$annee, "fin" => "31/12/".$annee),
-	);
-} else if ($periodicite === 12) {
-	$cotisations_a_creer = array(
-		array("debut" => "01/01/".$annee, "fin" => "31/12/".$annee),
-	);
-}
+// Si la case "Cotisation offerte" est cochée, on crée une cotisation
+// d'un montant de zéro.
+if ($cotisation_offerte) {
+	print "Adhérent $num_adherent : cotisation offerte.\n";
+	$montant = 0;
 
-foreach ($cotisations_a_creer as $cotis) {
-	$date_debut = dol_stringtotime($cotis["debut"]);
-	$date_fin = dol_stringtotime($cotis["fin"]);
-	// La date de paiement est le 5 du mois en début de période.
-	$date_paiement = dol_stringtotime(preg_replace("/^01/", "05", $cotis["debut"]));
-	$label = "Adhésion/cotisation $annee";
-	$accountid = 2; // Compte bancaire: CREDIT COOPERATIF
-	$emetteur_nom = '';
+	$date_debut = dol_stringtotime("01/01/".$annee);
+	$date_fin = dol_stringtotime("31/12/".$annee);
+	$accountid = 0;
+	$operation = '';
 	$num_chq = '';
+	$emetteur_nom = '';
 	$emetteur_banque = '';
-	$operation = 'PRE';
 
 	// Création de la cotisation
 	$cotis_id = $adherent->cotisation($date_debut, $montant, $accountid, $operation, $label, $num_chq, $emetteur_nom, $emetteur_banque, $date_fin);
 	if ($cotis_id <= 0) {
-		print "Erreur lors de la création de la cotisation : $member->error.\n";
-		$error = 1;
-		break;
+		throw new Exception("Erreur lors de la création de la cotisation : $member->error.\n");
+	}
+} else {
+	// Si la cotisation n'est pas offerte, c'est qu'elle doit être en
+	// prélèvement automatique (ce script ne peut pas gérer d'autres cas
+	// de figure).
+	// On vérifie les options du prélèvement automatique.
+	if (!$prelevement_auto_cotisation_euro && !$prelevement_auto_cotisation_eusko) {
+		throw new Exception("Erreur : cet adhérent ($num_adherent) n'a pas choisi le prélèvement automatique pour le paiement de sa cotisation.");
+	}
+	if ($prelevement_auto_cotisation_euro && $prelevement_auto_cotisation_eusko) {
+		throw new Exception("Erreur : pour cet adhérent ($num_adherent), les 2 prélèvements automatiques (€ et eusko) sont cochés pour le paiement de la cotisation.");
+	}
+	if ($montant <= 0 || !in_array($periodicite, array(1, 3, 6, 12))) {
+		throw new Exception("Erreur : le montant ($montant) ou la périodicité ($periodicite) du prélèvement est incorrect.\n");
 	}
 
-	// Création de l'écriture bancaire (option 'bankdirect')
-	$account = new Account($db);
-	$result = $account->fetch($accountid);
-	$insertid = $account->addline($date_paiement, $operation, $label, $montant, $num_chq, '', $user, $emetteur_nom, $emetteur_banque);
+	// Tous les paramètres ont été validés donc on crée les cotisations.
+	// La liste des cotisations à créer dépend de la périodicité choisie
+	// (1 = mensuel, 3 = trimestriel, 6 = semestriel, 12 = annuel).
+	if ($periodicite === 1) {
+		$cotisations_a_creer = array(
+			array("debut" => "01/01/".$annee, "fin" => "31/01/".$annee),
+			array("debut" => "01/02/".$annee, "fin" => "28/02/".$annee),
+			array("debut" => "01/03/".$annee, "fin" => "31/03/".$annee),
+			array("debut" => "01/04/".$annee, "fin" => "30/04/".$annee),
+			array("debut" => "01/05/".$annee, "fin" => "31/05/".$annee),
+			array("debut" => "01/06/".$annee, "fin" => "30/06/".$annee),
+			array("debut" => "01/07/".$annee, "fin" => "31/07/".$annee),
+			array("debut" => "01/08/".$annee, "fin" => "31/08/".$annee),
+			array("debut" => "01/09/".$annee, "fin" => "30/09/".$annee),
+			array("debut" => "01/10/".$annee, "fin" => "31/10/".$annee),
+			array("debut" => "01/11/".$annee, "fin" => "30/11/".$annee),
+			array("debut" => "01/12/".$annee, "fin" => "31/12/".$annee),
+		);
+	} else if ($periodicite === 3) {
+		$cotisations_a_creer = array(
+			array("debut" => "01/01/".$annee, "fin" => "31/03/".$annee),
+			array("debut" => "01/04/".$annee, "fin" => "30/06/".$annee),
+			array("debut" => "01/07/".$annee, "fin" => "30/09/".$annee),
+			array("debut" => "01/10/".$annee, "fin" => "31/12/".$annee),
+		);
+	} else if ($periodicite === 6) {
+		$cotisations_a_creer = array(
+			array("debut" => "01/01/".$annee, "fin" => "30/06/".$annee),
+			array("debut" => "01/07/".$annee, "fin" => "31/12/".$annee),
+		);
+	} else if ($periodicite === 12) {
+		$cotisations_a_creer = array(
+			array("debut" => "01/01/".$annee, "fin" => "31/12/".$annee),
+		);
+	}
 
-	if ($insertid <= 0) {
-		print "Erreur lors de la création de l'écriture bancaire : $acount->error.\n";
-		$error = 2;
-		break;
-	} else {
-		// Ajout de l'URL vers la fiche adhérent dans l'écriture bancaire
-		$inserturlid = $account->add_url_line($insertid, $adherent->id, DOL_URL_ROOT.'/adherents/fiche.php?rowid=', $adherent->getFullname($langs), 'member');
+	foreach ($cotisations_a_creer as $cotis) {
+		$date_debut = dol_stringtotime($cotis["debut"]);
+		$date_fin = dol_stringtotime($cotis["fin"]);
+		if ($prelevement_auto_cotisation_euro) {
+			$accountid = 2; // Compte bancaire: CREDIT COOPERATIF
+		} else if ($prelevement_auto_cotisation_eusko) {
+			$accountid = 4; // Compte bancaire: Compte numérique Eusko
+		}
+		$operation = 'PRE';
+		$num_chq = '';
+		$emetteur_nom = '';
+		$emetteur_banque = '';
 
-		if ($inserturlid <= 0) {
-			print "Erreur lors de l'ajout de l'URL vers la fiche Adhérent dans l'écriture bancaire : $account->error.\n";
-			$error = 3;
+		// Création de la cotisation
+		$cotis_id = $adherent->cotisation($date_debut, $montant, $accountid, $operation, $label, $num_chq, $emetteur_nom, $emetteur_banque, $date_fin);
+		if ($cotis_id <= 0) {
+			print "Erreur lors de la création de la cotisation : $member->error.\n";
+			$error = 1;
+			break;
+		}
+
+		// Création de l'écriture bancaire (option 'bankdirect')
+		// La date de paiement est le 5 du mois en début de période.
+		$date_paiement = dol_stringtotime(preg_replace("/^01/", "05", $cotis["debut"]));
+		$account = new Account($db);
+		$result = $account->fetch($accountid);
+		$insertid = $account->addline($date_paiement, $operation, $label, $montant, $num_chq, '', $user, $emetteur_nom, $emetteur_banque);
+
+		if ($insertid <= 0) {
+			print "Erreur lors de la création de l'écriture bancaire : $acount->error.\n";
+			$error = 2;
 			break;
 		} else {
-			// Création du lien entre la cotisation et l'enregistrement bancaire
-			$sql_majcotis = "UPDATE ".MAIN_DB_PREFIX."cotisation SET fk_bank=".$insertid;
-			$sql_majcotis .= " WHERE rowid=".$cotis_id;
-			$resql_majcotis = $db->query($sql_majcotis);
-			if (! $resql_majcotis) {
-				dol_print_error($db);
-				$error = 4;
+			// Ajout de l'URL vers la fiche adhérent dans l'écriture bancaire
+			$inserturlid = $account->add_url_line($insertid, $adherent->id, DOL_URL_ROOT.'/adherents/fiche.php?rowid=', $adherent->getFullname($langs), 'member');
+
+			if ($inserturlid <= 0) {
+				print "Erreur lors de l'ajout de l'URL vers la fiche Adhérent dans l'écriture bancaire : $account->error.\n";
+				$error = 3;
 				break;
+			} else {
+				// Création du lien entre la cotisation et l'enregistrement bancaire
+				$sql_majcotis = "UPDATE ".MAIN_DB_PREFIX."cotisation SET fk_bank=".$insertid;
+				$sql_majcotis .= " WHERE rowid=".$cotis_id;
+				$resql_majcotis = $db->query($sql_majcotis);
+				if (! $resql_majcotis) {
+					dol_print_error($db);
+					$error = 4;
+					break;
+				}
 			}
 		}
 	}
+}
+} catch (Exception $e) {
+	$error = 1;
+	print $e->getMessage()."\n";
 }
 
 // -------------------- END OF YOUR CODE --------------------
